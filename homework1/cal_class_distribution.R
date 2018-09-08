c0e2954fa96480eb4608649cd24a046fbb8ef9e4
Djackson <djackson@sjtu.edu.cn>
2018-09-08 00:19:27 -0500

    add mean calculation

--- homework1/cal_class_distribution.R
+++ homework1/cal_class_distribution.R
@@ -1,4 +1,19 @@
-cal_class_distribution <- function(inputData){
- res <- matrix(integer(), nrow = 10, ncol = length(inputData[1,]))
- 
+library(Matrix)
+g_u <- read.table("b_t.csv", sep=",")
+colnames(g_u)[1] = "Number"
+
+res <- matrix(0, nrow=10, ncol=length(g_u)-1)
+for (i in 0:9){
+  mat_set <- g_u[g_u[,"Number"]==i,]
+  for (j in 2:length(g_u)){
+    res[i+1,][j-1] <- round(sum(mat_set[,j])/nrow(mat_set))
+  }
 }
+for (i in 1:10){
+  mat <- matrix(res[i,], nrow=20)
+  Mat <- Matrix(mat)
+  im <- image(Mat)
+  print(im)
+}
+
+
