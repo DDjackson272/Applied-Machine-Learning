0e18534b63164ef622db90d338fd1d50fd3d4f03
Hengzhe Ding <916407217@qq.com>
2018-09-15 19:17:24 -0500

    data about right

--- homework2/hw2b.R
+++ homework2/hw2b.R
@@ -1,6 +1,6 @@
 pred_test = c()
 for (i in 1:nrow(test_data)){
-  if (sum(a*test_data[i,][1:4], na.rm=T)+b > 0)
+  if (sum(a*test_data[i,][c(1,3,5,11:13)])+b > 0)
     pred_test[i] <- "<=50K"
   else
     pred_test[i] <- ">50K"
