0207542ee40a13a841695b599ec07ee3fc961ff0
Hengzhe Ding <916407217@qq.com>
2018-09-22 10:42:19 -0500

    hw3 done

--- homework3/hw3.R
+++ homework3/hw3.R
@@ -1,5 +1,6 @@
 library(rgl)
 library(gsubfn)
+# Setting working directory and taking in data
 setwd("/Users/hengzhe/Library/Mobile Documents/com~apple~CloudDocs/UIUC/CS-498-Applied-Machine-Learning/homework3")
 iris.data <- read.table("./hw3-data/iris.csv", sep=",", header=T)
 iris.data1 <- read.table("./hw3-data/dataI.csv", sep=",", header=T)
@@ -7,6 +8,10 @@ iris.data2 <- read.table("./hw3-data/dataII.csv", sep=",", header=T)
 iris.data3 <- read.table("./hw3-data/dataIII.csv", sep=",", header=T)
 iris.data4 <- read.table("./hw3-data/dataIV.csv", sep=",", header=T)
 iris.data5 <- read.table("./hw3-data/dataV.csv", sep=",", header=T)
+
+# Setting parameters here
+pc.number <- 4
+use.noiesless <- TRUE
 
 # Function that returns error
 get_err <- function(input_mat1, input_mat2){
@@ -18,12 +23,30 @@ get_err <- function(input_mat1, input_mat2){
 }
 
 # Function that substract and store mean
-get_mean <- function(input_mat, output_mean){
+get_mean <- function(input_mat, output_mean, mean_of_mat){
   for (i in 1:length(input_mat)){
-    output_mean[i] <- mean(input_mat[,i])
-    input_mat[,i] <- input_mat[,i] - mean(input_mat[,i])
+    output_mean[i] <- mean(mean_of_mat[,i])
+    input_mat[,i] <- input_mat[,i] - mean(mean_of_mat[,i])
   }
   return (list(v1=input_mat, v2=output_mean))
+}
+
+# Function that returns reconstructed matrix.
+get_res <- function(input.list, eigen.vector){
+  x <- matrix(0, nrow = 150, ncol=4)
+  zero <- c(0,0,0,0)
+  for (i in 1:150){
+    r <- t(eigen.vector) %*% t(input.list$v1[i,])
+    if (0 < pc.number && pc.number < 4){
+      p <- c(r[1:pc.number], zero[(pc.number+1):4])
+    } else if (pc.number == 4) {
+      p <- r
+    } else if (pc.number == 0) {
+      p <- zero
+    }
+    x[i,] <- eigen.vector %*% p + input.list$v2
+  }
+  return (x)
 }
 
 # subtracting mean
@@ -33,12 +56,20 @@ x2.mean <- c()
 x3.mean <- c()
 x4.mean <- c()
 x5.mean <- c()
-list0 <- get_mean(iris.data, x.mean)
-list1 <- get_mean(iris.data1, x1.mean)
-list2 <- get_mean(iris.data2, x2.mean)
-list3 <- get_mean(iris.data3, x3.mean)
-list4 <- get_mean(iris.data4, x4.mean)
-list5 <- get_mean(iris.data5, x5.mean)
+list0 <- get_mean(iris.data, x.mean, iris.data)
+if (!use.noiesless){
+  list1 <- get_mean(iris.data1, x1.mean, iris.data1)
+  list2 <- get_mean(iris.data2, x2.mean, iris.data2)
+  list3 <- get_mean(iris.data3, x3.mean, iris.data3)
+  list4 <- get_mean(iris.data4, x4.mean, iris.data4)
+  list5 <- get_mean(iris.data5, x5.mean, iris.data5)
+} else {
+  list1 <- get_mean(iris.data1, x1.mean, iris.data)
+  list2 <- get_mean(iris.data2, x2.mean, iris.data)
+  list3 <- get_mean(iris.data3, x3.mean, iris.data)
+  list4 <- get_mean(iris.data4, x4.mean, iris.data)
+  list5 <- get_mean(iris.data5, x5.mean, iris.data)
+}
 
 # calculate covariance matrix
 cov_mat <- cov(list0$v1[,1:4])
@@ -57,36 +88,24 @@ eigen_vector4 <- eigen(cov_mat4)$vectors
 eigen_vector5 <- eigen(cov_mat5)$vectors
 
 #generate the final data
-x1 <- matrix(0, nrow = 150, ncol=4)
-x2 <- matrix(0, nrow = 150, ncol=4)
-x3 <- matrix(0, nrow = 150, ncol=4)
-x4 <- matrix(0, nrow = 150, ncol=4)
-x5 <- matrix(0, nrow = 150, ncol=4)
-pc.number <- 4
-zero <- c(0,0,0,0)
-for (i in 1:150){
-  r1 <- t(eigen_vector1) %*% t(list1$v1[i,])
-  r2 <- t(eigen_vector2) %*% t(list2$v1[i,])
-  r3 <- t(eigen_vector3) %*% t(list3$v1[i,])
-  r4 <- t(eigen_vector4) %*% t(list4$v1[i,])
-  r5 <- t(eigen_vector5) %*% t(list5$v1[i,])
-  if (pc.number < 4){
-    p1 <- c(r1[1:pc.number], zero[(pc.number+1):4])
-    p2 <- c(r2[1:pc.number], zero[(pc.number+1):4])
-    p3 <- c(r3[1:pc.number], zero[(pc.number+1):4])
-    p4 <- c(r4[1:pc.number], zero[(pc.number+1):4])
-    p5 <- c(r5[1:pc.number], zero[(pc.number+1):4]) 
-  } else {
-    p1 <- r1
-    p2 <- r2
-    p3 <- r3
-    p4 <- r4
-    p5 <- r5
-  }
-  x1[i,] <- eigen_vector1 %*% p1 + list1$v2
-  x2[i,] <- eigen_vector2 %*% p2 + list2$v2
-  x3[i,] <- eigen_vector3 %*% p3 + list3$v2
-  x4[i,] <- eigen_vector4 %*% p4 + list4$v2
-  x5[i,] <- eigen_vector5 %*% p5 + list5$v2
+if (!use.noiesless){
+  x1 <- get_res(list1, eigen_vector1)
+  x2 <- get_res(list2, eigen_vector2)
+  x3 <- get_res(list3, eigen_vector3)
+  x4 <- get_res(list4, eigen_vector4)
+  x5 <- get_res(list5, eigen_vector5)
+} else {
+  x1 <- get_res(list1, eigen_vector)
+  x2 <- get_res(list2, eigen_vector)
+  x3 <- get_res(list3, eigen_vector)
+  x4 <- get_res(list4, eigen_vector)
+  x5 <- get_res(list5, eigen_vector)
 }
 
+
+print(pc.number)
+print(get_err(x1, iris.data1))
+print(get_err(x2, iris.data2))
+print(get_err(x3, iris.data3))
+print(get_err(x4, iris.data4))
+print(get_err(x5, iris.data5))
