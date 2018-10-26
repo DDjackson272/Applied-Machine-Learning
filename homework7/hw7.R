ac6644e51f5159406f5fa9cd8d4dd6b4f8f27c44
Hengzhe Ding <916407217@qq.com>
2018-10-26 15:38:08 -0500

    hw7 half-done

---
+++ homework7/hw7.R
@@ -0,0 +1,42 @@
+library(glmnet)
+library(dplyr)
+setwd("/Users/hengzhe/Library/Mobile Documents/com~apple~CloudDocs/UIUC/CS-498-Applied-Machine-Learning/homework7")
+train_blog <- read.table("./BlogFeedback/blogData_train.csv", sep=",")
+test_folder <- list.files("./BlogFeedback/test")
+
+get_all_data <- function(input_file_list, input_file_path){
+  temp <- read.table(paste(input_file_path, input_file_list[1], sep=""), sep=",")
+  for (i in 2:length(input_file_list)){
+    temp_origin <- read.table(paste(input_file_path, input_file_list[i], sep=""), sep=",")
+    # print(length(temp_origin))
+    temp <- rbind(temp, temp_origin)
+  }
+  rownames(temp) <- 1:nrow(temp)
+  return (temp)
+}
+
+test_blog <- get_all_data(test_folder, "./BlogFeedback/test/")
+
+# problem a
+y <- as.matrix(train_blog[,281])
+x <- as.matrix(train_blog[,1:280])
+mod <- cv.glmnet(x,y,family="poisson",alpha=1)
+plot(mod)
+
+# problem b
+fit1 <- predict(mod, x, s = "lambda.min")
+fit2 <- predict(mod, x, s = "lambda.1se")
+plot(y, fit1, xlab="True value", ylab="Fitted value", 
+     main="Fitted value vs True value")
+plot(y, fit2, xlab="True value", ylab="Fitted value", 
+     main="Fitted value vs True value")
+
+# problem c
+test_x <- as.matrix(test_blog[,1:280])
+test_y <- as.matrix(test_blog[,281])
+fit_test_1 <- predict(mod, test_x, s="lambda.min")
+fit_test_2 <- predict(mod, test_x, s="lambda.1se")
+plot(test_y, fit_test_1, xlab="True value", ylab="Fitted value", 
+     main="Fitted value vs True value")
+plot(test_y, fit_test_2, xlab="True value", ylab="Fitted value", 
+     main="Fitted value vs True value")
